7c00c7f480e9e3be379a07d2fe0604cca23b561f
J0aoCunha <jvjunger20123@gmail.com>
2025-04-04 23:14:19 -0300

    first commit

---
+++ eslint.config.mjs
@@ -0,0 +1,36 @@
+import { defineConfig } from 'eslint/config';
+import globals from 'globals';
+import js from '@eslint/js';
+import tseslint from 'typescript-eslint';
+
+
+export default defineConfig([
+  {
+    files: ['**/*.{js,mjs,cjs,ts}'],
+  },
+  {
+    files: ['**/*.{js,mjs,cjs,ts}'],
+    languageOptions: { globals: globals.browser },
+  },
+  {
+    files: ['**/*.{js,mjs,cjs,ts}'],
+    plugins: { js },
+    extends: ['js/recommended'],
+  },
+  tseslint.configs.recommended,
+
+  {
+    rules: {
+      'quotes': ['error', 'single'],
+      'semi': ['error', 'always'],
+      'indent': ['error', 2],
+      'no-trailing-spaces': 'error',
+      'eol-last': ['error', 'always'],
+      'curly': ['error', 'all'],
+      'comma-dangle': ['error', 'always-multiline'],
+      'arrow-parens': ['error', 'always'],
+      'max-len': ['error', { code: 80, tabWidth: 2, ignoreUrls: true }],
+      'space-before-function-paren': ['error', 'never'],
+    },
+  },
+]);
